b9c6482c081c4474007061cb3c62aaba9635b131
Paez Gabriel <gabrielpaez043@gmail.com>
2024-05-16 13:46:13 -0300

    Agregar un titulo

--- src/Banco.java
+++ src/Banco.java
@@ -1,5 +1,6 @@
 public class Banco {
     public static void main(String[] args) throws Exception {
+        System.out.println("Datos Bancarios:");
         System.out.println("Total Ganancia = $1500M");
     }
 }
